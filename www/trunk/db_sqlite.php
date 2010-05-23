7a6c124428acc27ec1f7f78b1141cd6429bfdbc1
jeffreygriffindor <jeffreygriffindor@277be6ea-87b5-9aff-e0a1-898548d85874>
2010-05-23 23:55:37 +0000

    www/db_sqlite.php: moved 'for debugging' statement back to top (moving to 		    bottom its purpose

--- www/trunk/db_sqlite.php
+++ www/trunk/db_sqlite.php
@@ -1,5 +1,8 @@
 <?php
 
+// for debugging
+//include('db.php');
+
 /* class SQLiteDBObject provides a base class for all classes which
    represent objects in a SQLite database
 */
@@ -345,6 +348,4 @@ filename=test/SQLiteTest.db
   }
 }
 
-// for debugging
-// include('db.php');
 //SQLiteTest::testAll();
