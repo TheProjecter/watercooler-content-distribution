dab109ad088437b9608888df46461fb3a73f1fa1
jeffreygriffindor <jeffreygriffindor@277be6ea-87b5-9aff-e0a1-898548d85874>
2010-05-24 22:47:14 +0000

    www/index.php: now uses auth.php for session authentication

--- www/trunk/index.php
+++ www/trunk/index.php
@@ -1,6 +1,6 @@
 <?php
-session_start();
-if (!isset($_SESSION['userName'])) {
+require_once('auth.php');
+if (!isset($user)) {
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
