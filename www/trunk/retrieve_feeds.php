71ff982d9c59be26e95fb3b09b3252b6e448a710
tripledouble1210 <tripledouble1210@277be6ea-87b5-9aff-e0a1-898548d85874>
2010-05-28 01:08:02 +0000

    retrieve_feeds.php:  added a bit of skeleton code to this file, whose purpose is to retrieve the most recent stories for a given feedSource, and to format those stories for display in the feed reader.

---
+++ www/trunk/retrieve_feeds.php
@@ -0,0 +1,14 @@
+<?php
+include_once('db_init.php');
+include_once('auth.php');
+
+function getFeeds($feedSource) {
+  if (iFeed::get() != NULL)
+    {
+      //fail gracefully
+    }
+  else
+    {
+      
+    }
+}
