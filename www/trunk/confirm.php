18de7c47463f040ba64e76e4f66024bd2020820e
tripledouble1210 <tripledouble1210@277be6ea-87b5-9aff-e0a1-898548d85874>
2010-06-04 17:40:42 +0000

    made the homepage look nicer

--- www/trunk/confirm.php
+++ www/trunk/confirm.php
@@ -7,6 +7,8 @@ if (isset($_REQUEST['id']) && isset($_REQUEST['pin']))
       && $user->email_pin === $_REQUEST['pin'])
     $user->email_confirmed = TRUE;
 
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html lang="EN" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
