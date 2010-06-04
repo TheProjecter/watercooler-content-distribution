22f9948c2971ce15bb4937a04e88d745e3e81196
jeffreygriffindor <jeffreygriffindor@277be6ea-87b5-9aff-e0a1-898548d85874>
2010-06-04 09:19:48 +0000

    www/auth.php: now checks for user email confirmation

--- www/trunk/auth.php
+++ www/trunk/auth.php
@@ -7,9 +7,11 @@ session_start();
 if (isset($_SESSION['uid']) && isset($_SESSION['password'])) {
   // find user in database
   $user = User::find('uid', $_SESSION['uid']);
-  // end session if username does not exist or password does not match
+  /* end session if username does not exist or password does not match
+     or user is not confirmed by email */
   if ($user === NULL
-      || $_SESSION['password'] != $user->password) {
+      || $_SESSION['password'] != $user->password
+      || !$user->email_confirmed) {
     unset($_SESSION['uid']);
     unset($_SESSION['password']);
   }
