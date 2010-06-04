0bb205bd3548e185bf3e630052a4f4c8f67b7961
jeffreygriffindor <jeffreygriffindor@277be6ea-87b5-9aff-e0a1-898548d85874>
2010-06-04 09:11:25 +0000

    www/db_mysql: fixed support for 'phone_confirmed' and 'email_confirmed' www: confirm.php now works

--- www/trunk/confirm.php
+++ www/trunk/confirm.php
@@ -3,7 +3,6 @@
 require_once('db_init.php');
 
 if (isset($_REQUEST['id']) && isset($_REQUEST['pin']))
-  if (($user = User::find('id', $_REQUEST['id'])) !== NULL)
-    // XXX validate pin and confirm user
-    //$user->confirmed = TRUE;
-    echo 'confirmed';
+  if (($user = User::find('id', $_REQUEST['id'])) !== NULL
+      && $user->email_pin === $_REQUEST['pin'])
+    $user->email_confirmed = TRUE;
